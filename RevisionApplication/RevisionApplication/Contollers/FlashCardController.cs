c4ef28cd37f73f97c28c9877494b8e157c908154
Matt Broomer <matt_broomer@hotmail.com>
2020-02-24 11:38:41 +0000

    Add initial flash card functionality.

---
+++ RevisionApplication/RevisionApplication/Contollers/FlashCardController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RevisionApplication.Models;
+using RevisionApplication.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RevisionApplication.Contollers
+{
+    public class FlashCardController : Controller
+    {
+        private readonly IQuestionRepository _questionRepository;
+
+        public FlashCardController(IQuestionRepository questionRepository)
+        {
+            _questionRepository = questionRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Index(int record)
+        {
+
+            var question = _questionRepository.GetAllQuestions().Where(p => p.Id > record).OrderBy(p => p.Id).FirstOrDefault();
+
+            if (question is null)
+            {
+                return RedirectToAction("Index", "FlashCard", new { record = 0 });
+            }
+
+            var revisionViewModel = new RevisionViewModel()
+            {
+                Title = "Flash card question",
+                Question = question,
+                currentRecord = record
+            };
+
+            return View(revisionViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Index(RevisionViewModel model)
+        {
+            model.Title = "Flash card answer"; 
+            return View("Answer", model);
+        }
+
+
+        [HttpPost]
+        public IActionResult Answer(RevisionViewModel model, int record)
+        {
+            return RedirectToAction("Index", "FlashCard", new { record = model.Question.Id });
+        }
+    }
+}
--- RevisionApplication/RevisionApplication/Models/DbInitializer.cs
+++ RevisionApplication/RevisionApplication/Models/DbInitializer.cs
@@ -9,13 +9,15 @@ namespace RevisionApplication.Models
     {
         public static void Seed(AppDbContext context)
         {
-            // V1 
+            // V2 
             if (!context.Questions.Any())
             {
                 context.AddRange
                     (
-                        new Question { Content = "Question 1" },
-                        new Question { Content = "Question 2" }
+                        new Question { Content = "Question 1", Answer1 = "true", Answer2 = "false", Answer3 = "false", Answer4 = "false", CorrectAnswer = 1, Reference = "Reference 1" },
+                        new Question { Content = "Question 2", Answer1 = "false", Answer2 = "true", Answer3 = "false", Answer4 = "false", CorrectAnswer = 2, Reference = "Reference 2" },
+                        new Question { Content = "Question 3", Answer1 = "false", Answer2 = "false", Answer3 = "true", Answer4 = "false", CorrectAnswer = 3, Reference = "Reference 3" },
+                        new Question { Content = "Question 4", Answer1 = "false", Answer2 = "false", Answer3 = "false", Answer4 = "true", CorrectAnswer = 4, Reference = "Reference 4" }
                     );
 
                 context.SaveChanges();
