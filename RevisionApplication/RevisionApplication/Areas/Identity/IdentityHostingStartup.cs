de23bfd236e478d0a5f2b403b5f4ee934a19ec13
Matt Broomer <matt_broomer@hotmail.com>
2020-04-16 16:40:31 +0100

    Remove unused pages.

--- RevisionApplication/RevisionApplication/Areas/Identity/IdentityHostingStartup.cs
+++ RevisionApplication/RevisionApplication/Areas/Identity/IdentityHostingStartup.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using RevisionApplication.Models;
 using RevisionApplication.Repository;
 
 [assembly: HostingStartup(typeof(RevisionApplication.Areas.Identity.IdentityHostingStartup))]
